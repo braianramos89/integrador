b66ca616c90ad6bf81188648522d6309cfd04420
Braian Ramos <bramos@fie.undef.edu.ar>
2022-05-30 23:24:42 -0300

    agregado de los archivos js para trabajo integrador de fromend

---
+++ js/main.js
@@ -0,0 +1,43 @@
+function captura(){
+    var nombrecompra=document.getElementById("Nombre").value;
+    var apellidocompra=document.getElementById("Apellido").value;
+    var cantidadcompra=document.getElementById("Cantidad").value;
+    var variedadcompra=document.getElementById("Option").value;
+    if (cantidadcompra==""){
+        alert ("La cantidad es necesaria");
+        document.getElementById("Cantidad").focus();
+    }
+    else{
+        if (variedadcompra=="Estudiante"){
+            var resumen=cantidadcompra * 0.2 * 200;
+            document.getElementById("resultado").value = resumen;
+        }
+        else{
+            if (variedadcompra=="Trainee"){
+                var resumen=cantidadcompra * 0.5 * 200;
+                document.getElementById("resultado").value = resumen;
+            }
+            else{
+                var resumen=cantidadcompra * 0.85 * 200;
+                document.getElementById("resultado").value = resumen;
+            }
+        }
+
+    }
+}
+
+function borrar() {
+    document.getElementById("Nombre").value = "";
+    document.getElementById("Apellido").value = "";
+    document.getElementById("resultado").value = "";
+    document.getElementById("Cantidad").value = "";
+    document.getElementById("Email").value = "";
+}
+
+function validarEmail (Email) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/.test (Email)){
+        alert("La dirección de email " + Email + " es correcta.");
+    } else {
+        alert("La dirección de email es incorrecta.");
+    }
+}
